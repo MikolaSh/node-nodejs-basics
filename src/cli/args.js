b0258d53969522dbf949acf91f165eebc007563a
Mikola <nikolashulga2000@gmail.com>
2025-04-28 01:30:30 +0300

    feat: implemets cli tasks solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv.slice(2);
+
+    const pairs = args.flatMap((_, index, array) => {
+        return index % 2 ? [] : [array.slice(index, index + 2)]
+    });
+
+    const result = pairs.map((pair) => {
+        const [key, value] = pair;
+        return `${key.split('--')[1]} is ${value}`;
+    })
+
+    console.log(result.join(', '));
 };
 
 parseArgs();
