6edecb6e43f492bf180bb9d7d971213250781e20
Mikola <nikolashulga2000@gmail.com>
2025-04-28 22:31:35 +0300

    feat: implements transform tasks solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const pathToFile = __dirname + '/files/fileToWrite.txt';
+
+    const stream = fs.createReadStream(pathToFile);
+
+    stream.on('data', (data) => {
+        process.stdout.write(data + "\n")
+    });
+
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from "stream"; 
+
 const transform = async () => {
-    // Write your code here 
+
+    const revers = new Transform({
+        transform(chunk, _, callback) {
+            const reversedString = chunk.toString().split("").reverse().join("")
+            this.push(reversedString);
+            callback();
+        }
+    })
+
+    process.stdin.pipe(revers).pipe(process.stdout);
 };
 
 await transform();
