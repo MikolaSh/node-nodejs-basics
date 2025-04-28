072e364abbc028d743554dc322a9bdeaa94afb11
Mikola <nikolashulga2000@gmail.com>
2025-04-28 21:39:47 +0300

    feat: implements hash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import crypto from "crypto";
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const stream = fs.createReadStream(__dirname + '/files/fileToCalculateHashFor.txt');
+
+    const hash = crypto.createHash('sha256');
+
+    hash.setEncoding('hex');
+    
+    
+    stream.pipe(hash);
+
+    stream.on('end', () => {
+        hash.end();
+        console.log(hash.read());
+    });
+
 };
 
 await calculateHash();
